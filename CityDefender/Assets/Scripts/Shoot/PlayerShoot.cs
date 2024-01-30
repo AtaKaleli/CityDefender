4923bfe9e1e89c6da25f41aecb40120f2197b2dc
ata kaleli <atakaleli123@gmail.com>
2024-01-30 21:31:55 +0300

    SpecialShootAdded

--- CityDefender/Assets/Scripts/Shoot/PlayerShoot.cs
+++ CityDefender/Assets/Scripts/Shoot/PlayerShoot.cs
@@ -8,20 +8,37 @@ public class PlayerShoot : MonoBehaviour
     
     private Rigidbody2D _rb;
     public float _flingForce = 500;
+    private float _timer;
+    private float _specialShootTime = 3;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _timer = _specialShootTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        _timer += Time.deltaTime;
+        
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GameObject newShoot = Instantiate(_playerShootPrefab, transform.position + new Vector3(0.5f,0,0), Quaternion.identity);
             Rigidbody2D shootRB = newShoot.GetComponent<Rigidbody2D>();
             shootRB.AddForce(new Vector2(_flingForce, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.F) && _timer >= _specialShootTime)
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+                float yCoordinate = i - 9.5f;
+                GameObject newShoot = Instantiate(_playerShootPrefab, transform.position + new Vector3(0.5f, yCoordinate, 0), Quaternion.identity);
+                Rigidbody2D shootRB = newShoot.GetComponent<Rigidbody2D>();
+                shootRB.AddForce(new Vector2(_flingForce, 0)); // Adjust the force or any other parameters as needed
+            }
+            _timer = 0;
         }
     }
 }
