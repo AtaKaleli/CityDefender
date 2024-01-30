33fe4283f620ca95cc7291ae17faaca10eb58875
ata kaleli <atakaleli123@gmail.com>
2024-01-30 13:28:26 +0300

    GithubTry

--- CityDefender/Assets/Scripts/CharacterMovement.cs
+++ CityDefender/Assets/Scripts/CharacterMovement.cs
@@ -7,7 +7,7 @@ public class CharacterMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("github try");
     }
 
     // Update is called once per frame
