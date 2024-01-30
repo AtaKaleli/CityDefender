0372f4d73c1f8b040d308d2bfbfde8489bc3f1a1
ata kaleli <atakaleli123@gmail.com>
2024-01-30 14:24:58 +0300

    CharacterMovementAdded

--- CityDefender/Assets/Scripts/CharacterMovement.cs
+++ CityDefender/Assets/Scripts/CharacterMovement.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    
+    private Rigidbody2D _rb;
+    private float _moveSpeed = 5;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");  
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 moveInput = new Vector3(horizontalInput, verticalInput, 0);
+        Vector3 nextMove = transform.position + moveInput * Time.deltaTime * _moveSpeed;
+        _rb.MovePosition(nextMove);
+    }
+
+
+
 }
