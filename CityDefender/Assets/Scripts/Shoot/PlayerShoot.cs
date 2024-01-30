66fe45ece6722e45c2618f22e134420db5f3cfb2
ata kaleli <atakaleli123@gmail.com>
2024-01-30 19:12:06 +0300

    PlayerShootAdded

--- CityDefender/Assets/Scripts/Shoot/PlayerShoot.cs
+++ CityDefender/Assets/Scripts/Shoot/PlayerShoot.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject _playerShootPrefab;
+    
+    private Rigidbody2D _rb;
+    public float _flingForce = 500;
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject newShoot = Instantiate(_playerShootPrefab, transform.position + new Vector3(0.5f,0,0), Quaternion.identity);
+            Rigidbody2D shootRB = newShoot.GetComponent<Rigidbody2D>();
+            shootRB.AddForce(new Vector2(_flingForce, 0));
+        }
     }
 }
